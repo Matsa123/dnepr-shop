8450eb220de4e054154f595decfd3c75a80eb3a0
Dmytro Matsulevych <matsa@Noutbuk-Dmytro.local>
2025-04-21 12:41:46 +0200

    Header and Footer 2

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,23 +1,25 @@
 <footer class="site-footer">
     <div class="footer-container">
+
         <div class="footer-block">
             <h3 class="footer-title">Dnepr Shop</h3>
             <p>Одяг з доставкою по Україні</p>
         </div>
+        <div class="footer-blocks">
+            <div class="footer-block">
+                <h4>Навігація</h4>
+                <ul>
+                    <li><a href="#">Про нас</a></li>
+                    <li><a href="#">Доставка</a></li>
+                    <li><a href="#">Політика конфіденційності</a></li>
+                </ul>
+            </div>
 
-        <div class="footer-block">
-            <h4>Навігація</h4>
-            <ul>
-                <li><a href="#">Про нас</a></li>
-                <li><a href="#">Доставка</a></li>
-                <li><a href="#">Політика конфіденційності</a></li>
-            </ul>
-        </div>
-
-        <div class="footer-block">
-            <h4>Контакти</h4>
-            <p>Email: info@dneprshop.ua</p>
-            <p>Телефон: +38 (099) 123-45-67</p>
+            <div class="footer-block">
+                <h4>Контакти</h4>
+                <p>Email: info@dneprshop.ua</p>
+                <p>Телефон: +38 (099) 123-45-67</p>
+            </div>
         </div>
     </div>
     <div class="footer-bottom">
