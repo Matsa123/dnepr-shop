e6c6354d85a1ef94fa176146a13152d518533cbd
Dmytro Matsulevych <matsa@Noutbuk-Dmytro.local>
2025-05-08 18:08:23 +0200

    Catalog_css

--- resources/views/partials/product-list.blade.php
+++ resources/views/partials/product-list.blade.php
@@ -1,8 +1,13 @@
 @forelse($products as $product)
     <div class="product-card">
         <img src="{{ $product->image }}" alt="{{ $product->name }}">
-        <h3>{{ $product->name }}</h3>
-        <p>{{ $product->price }} грн</p>
+        <div class="product-card-content">
+            <div class="product-info">
+                <h3>{{ $product->name }}</h3>
+                <p>{{ $product->price }} грн</p>
+            </div>
+            <a href="#" class="buy-now-btn">Купити зараз</a>
+        </div>
     </div>
 @empty
     <p>Нічого не знайдено</p>
